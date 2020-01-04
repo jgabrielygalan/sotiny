2940f3d7fd1c2cdb1546aea3ec1bac4ab2016214
dxfl <thedixie@gmail.com>
2020-01-04 18:34:40 +0100

    add draft method

--- sotiny.py
+++ sotiny.py
@@ -1,41 +1,50 @@
 
 import os
-import discord
+import random
+
+CUBE_CARDS = 'EternalPennyDreadfulCube.txt'
 
 from dotenv import load_dotenv
+
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='>>>')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
     
+@bot.command(name='reparte', help='reparte cartas')
+async def repartir(ctx):
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    with open(CUBE_CARDS) as f:
+        read_cards = f.read().splitlines()
 
-    if message.content == '>>>repartecartas':
-        response = 'momentito estoy barajando...'
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-        
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+    cube = read_cards
+    players = 4
+    draft = {}
+
+    for p in range(players):
+        draft[f'player_{p}'] = []
+
+        for i in range(15):
+            card = cube.pop(random.randint(0, len(cube)))
+            draft[f'player_{p}'].append(card)
+
+
+    response = "\n".join(draft['player_0'])
+
     
+    await ctx.send(response)
     
 #client.run(TOKEN)
 
-client.run('NjYzMDI3MjY5NjAxNTkxMzI3.XhCjYg.V4f6J8u9KmPHJhrXkXkcmCSnQyw')
+
+
+
+bot.run('NjYzMDI3MjY5NjAxNTkxMzI3.XhCjYg.V4f6J8u9KmPHJhrXkXkcmCSnQyw')
