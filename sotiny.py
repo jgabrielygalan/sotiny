43f95250555f0abcc806fce799491a685d92488c
dxfl <thedixie@gmail.com>
2020-01-04 17:03:54 +0100

    connect, get name of the guild, and users

--- sotiny.py
+++ sotiny.py
@@ -12,7 +12,17 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+        
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members: \n - {members}')
+    
 
 
     
