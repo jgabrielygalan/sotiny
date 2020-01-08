05898d428d6c8a37ba283e8feae150945a017c0c
Jesus Gabriel y Galan <jesus.gabrielygalan@docomodigital.com>
2020-01-08 14:38:26 +0100

    pass_booster

--- draft.py
+++ draft.py
@@ -1,5 +1,5 @@
 import random
-import booster
+from booster import Booster
 
 class Draft:
 
@@ -7,16 +7,17 @@ class Draft:
 
 	def __init__(self, players, file_name=FILE_NAME):
 		self.file_name = file_name
-		self.players = random.shuffle(players)
+		self.players = players
+		random.shuffle(self.players)
 		self.state = {}
 		self.decks = { player:[] for player in players }
 		
 	def start(self):
 		self.cards = get_cards(self.file_name)
-		for player in players:
+		for player in self.players:
 			card_list = []
 			for i in range(15):
-				card_list.append(cards.pop(random.randint(0, len(cards))))
+				card_list.append(self.cards.pop(random.randint(0, len(self.cards))))
 			self.state[player] = Booster(card_list)
 		self.booster_number = 1
 		self.picked = []
@@ -28,18 +29,20 @@ class Draft:
 			self.state[player].pick(card_name)
 			self.decks[player].append(card_name)
 			self.picked.append(player)
-		if len(picked) == len(self.players):
+		if len(self.picked) == len(self.players):
+			print("all players picked")
 			self.picked = []
 			if len(self.players[0]) > 0:
+				print("pass booster")
 				self.pass_boosters()
 			else:
+				print("open new booster")
 				self.open_next_booster()
 			return self.state
 		return None
 
 	def pass_boosters(self):
-		self.state = { list(self.state)[i]: list(self.state.values())[i + (-1)^self.booster_number] for i in range(len(self.state)) }
-		
+		self.state = { list(self.players)[i + 1*(-1)^self.booster_number]: self.state[self.players[i]] for i in range(len(self.players)) }
 
 	def show_deck(self, player):
 		return decks[player]
@@ -53,9 +56,20 @@ def get_cards(file_name):
 
 
 def main():
-	draft = Draft(4)
-	cube = draft.get_cards()
-	draft.show_decks(cube)
+	players = ['a', 'b', 'c', 'd']
+	draft = Draft(players)
+	packs = draft.start()
+	for i in range(1,45):
+		for p in players:
+			print("{player} deck: {cards}".format(player=p,cards=draft.decks[p]))
+			print("{player}: {cards}".format(player=p,cards=packs[p].cards))
+
+		for p in players:
+			new_packs = draft.pick(p, packs[p].cards[0])
+		packs = new_packs
+
+	#cube = draft.get_cards()
+	#draft.show_decks(cube)
 	#print(draft.deal_cards(cube))
 
 if __name__ == "__main__":
