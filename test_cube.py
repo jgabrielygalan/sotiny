c0fffb54ac9571ee08c8b03ff81e44dfba67b3e2
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-01-27 16:45:10 +1100

    Oh, tests

---
+++ test_cube.py
@@ -0,0 +1,14 @@
+import pytest
+import cube
+
+@pytest.mark.vcr(record_mode='new_episodes')
+@pytest.mark.asyncio
+async def test_long_id():
+    fun = await cube.load_cubecobra_cube("5d7488c28d85b07c66d22162")
+    assert "Fun with Multiples" == fun.name
+
+
+@pytest.mark.vcr(record_mode='new_episodes')
+@pytest.mark.asyncio
+async def test_a93():
+    a93 = await cube.load_cubecobra_cube("a93")
