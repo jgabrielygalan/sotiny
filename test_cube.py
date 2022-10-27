a168c5bfe0d4a055134fdb1106d258c7ba93ccd6
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-10-27 15:26:03 +1100

    Add test

--- test_cube.py
+++ test_cube.py
@@ -5,7 +5,7 @@ import cube
 
 @pytest.mark.vcr(record_mode='new_episodes')
 @pytest.mark.asyncio
-async def test_long_id():
+async def test_long_id() -> None:
     fun = await cube.load_cubecobra_cube("5d7488c28d85b07c66d22162")
     await fun.ensure_data()
     assert "Fun with Multiples" == fun.name
@@ -13,7 +13,11 @@ async def test_long_id():
 
 @pytest.mark.vcr(record_mode='new_episodes')
 @pytest.mark.asyncio
-async def test_a93():
+async def test_a93() -> None:
     a93 = await cube.load_cubecobra_cube("a93")
     assert a93.cards
 
+@pytest.mark.vcr(record_mode='new_episodes')
+@pytest.mark.asyncio
+async def test_mdfc_colors() -> None:
+    await cube.fetch_card('Jwari Disruption // Jwari Ruins')
